604ada1a11a50688a978ed1196ab4b8079230ab1
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-02-28 11:16:37 +0700

    DELETE

--- UPLOAD/resources/js/portfolio/components/NoiseOverlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoiseOverlay: React.FC = () => (
-    <div className="fixed inset-0 pointer-events-none z-[99] opacity-[0.03] bg-[url('https://grainy-gradients.vercel.app/noise.svg')]" />
-);
-
-export default NoiseOverlay;
